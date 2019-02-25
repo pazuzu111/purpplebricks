b238125c89e2406ccfcf25599472134dec28326e
darc window <darc.windowwww@gmail.com>
2019-02-24 21:34:41 -0500

    adding tests for reducer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reducers from './reducers/reducers'
+import { GET_CARDS, RECEIVE_CARDS, FILTER_CARDS } from './actions/actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('fetch reducer', () => {
+  it('has a default state', () => {
+    expect(reducers(undefined, {}))
+    .toEqual({ cards: []})
+  })
+  
+  const cards = [{data: {title: 'one'}}]
+  it('handles GET_CARDS request', () => {
+    expect(reducers(undefined, {
+      type:   GET_CARDS,
+      payload: {
+       name: ''
+      }
+    })).toEqual({
+      loading: true,
+      cards: []
+    })
+  })
+
+  it('handles RECEIVE_CARDS request', () => {
+    expect(reducers([], {
+      type:   RECEIVE_CARDS,
+      payload: {
+        name: '',
+        cards: cards
+      }
+    })).toEqual({
+      loading: false
+    })
+  })
+
+  it('handles FILTER_CARDS request', () => {
+    expect(reducers(undefined, {
+      type:   FILTER_CARDS,
+      payload: {
+        pokemonType: 'mega'
+      }
+    })).toEqual({
+      loading: true,
+      cards: []
+    })
+  } )
+})
